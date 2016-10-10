9be41524fa35183189d8c17bc87f686c499390e5
Abhishek Chowdhary <andromobs5@gmail.com>
2016-10-09 22:18:56 -0700

    Add files via upload

---
+++ javascript/master.js
@@ -0,0 +1,12 @@
+$(function(){
+
+$('.1').animate({width:'90%'},1500);
+$('.2').animate({width:'85%'},2000);
+$('.3').animate({width:'80%'},2500);
+$('.4').animate({width:'80%'},3000);
+$('.5').animate({width:'70%'},3500);
+$('.6').animate({width:'70%'},4000);
+$('.7').animate({width:'50%'},4500);
+$('.8').animate({width:'50%'},5000);
+
+});
